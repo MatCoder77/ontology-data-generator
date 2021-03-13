0a5a779cd15b9d75c2627eeab46267f93593eed5
MatCoder77 <matkimzag@wp.pl>
2021-03-13 19:49:39 +0100

    Initial commit

---
+++ src/main/java/pl/edu/pwr/ontologydatagenerator/OntologyDataGeneratorApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.ontologydatagenerator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OntologyDataGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OntologyDataGeneratorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pwr/ontologydatagenerator/OntologyDataGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pwr.ontologydatagenerator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OntologyDataGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
